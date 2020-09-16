fc6142c7ae6f1e72076364af0948af37cdf57fcd
DiveFish <DiveFish@users.noreply.github.com>
2020-09-16 11:47:16 +0200

    Fix test config

--- dpar-utils/src/config_tests.rs
+++ dpar-utils/src/config_tests.rs
@@ -7,14 +7,14 @@ lazy_static! {
         parser: Parser {
             pproj: true,
             system: String::from("stackproj"),
-            inputs: String::from("parser.inputs"),
-            transitions: String::from("parser.transitions"),
+            inputs: String::from("inputs/parser.inputs"),
+            transitions: String::from("inputs/parser.transitions"),
             train_batch_size: 8192,
             parse_batch_size: 8192,
         },
         model: Model {
             graph: String::from("parser.graph"),
-            parameters: String::from("params"),
+            parameters: String::from("parameters/epoch-0000"),
             intra_op_parallelism_threads: 2,
             inter_op_parallelism_threads: 2,
             allow_growth: true,
@@ -27,21 +27,21 @@ lazy_static! {
         },
         lookups: Lookups {
             word: Some(Lookup::Embedding {
-                filename: String::from("word-vectors.bin"),
+                filename: String::from("word-vectors.fifu"),
                 op: String::from("model/tokens"),
                 embed_op: String::from("model/token_embeds"),
             }),
             tag: Some(Lookup::Embedding {
-                filename: String::from("tag-vectors.bin"),
+                filename: String::from("tag-vectors.fifu"),
                 op: String::from("model/tags"),
                 embed_op: String::from("model/tag_embeds"),
             }),
             deprel: Some(Lookup::Table {
-                filename: String::from("deprels.lookup"),
+                filename: String::from("inputs/deprels.lookup"),
                 op: String::from("model/deprels"),
             }),
             feature: Some(Lookup::Table {
-                filename: String::from("features.lookup"),
+                filename: String::from("inputs/features.lookup"),
                 op: String::from("model/features"),
             }),
         }
